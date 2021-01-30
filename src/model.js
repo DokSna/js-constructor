c7a3127340c7b0d1faec051af99721012359956c
Artem <31787423+DokSna@users.noreply.github.com>
2021-01-30 18:59:19 +0300

    создание функций row & col

--- src/model.js
+++ src/model.js
@@ -1,6 +1,8 @@
+import image from './assets/image.png';
+
 export const model = [
   { type: 'title', value: 'Hello World from JS!!!' },
   { type: 'text', value: 'here we go some text' },
   { type: 'columns', value: ['111111', '222222', '333333', '444444'] },
-  { type: 'image', value: './assets/image.png' },
+  { type: 'image', value: image },
 ];
