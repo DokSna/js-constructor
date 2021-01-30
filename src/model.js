32654a07797428ce8a6c7cbd8356d041d1e75a30
Artem <31787423+DokSna@users.noreply.github.com>
2021-01-30 22:32:55 +0300

    Добавлены стили к объектам

--- src/model.js
+++ src/model.js
@@ -1,5 +1,7 @@
 import image from './assets/image.png';
 
+const text = `Крутые видео и уроки по JavaScript.`;
+
 export const model = [
   {
     type: 'title',
@@ -18,7 +20,48 @@ export const model = [
       },
     },
   },
-  { type: 'text', value: 'here we go some text' },
-  { type: 'columns', value: ['111111', '222222', '333333', '444444'] },
-  { type: 'image', value: image },
+  {
+    type: 'image',
+    value: image,
+    options: {
+      styles: {
+        padding: '2rem 0',
+        display: 'flex',
+        'justify-content': 'center',
+      },
+      imageStyles: {
+        width: '500px',
+        height: 'auto',
+      },
+      alt: 'Это картинка',
+    },
+  },
+  {
+    type: 'columns',
+    value: [
+      'Приложение на чистом JavaScript, без использования библиотек',
+      'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+      'JavaScript - это просто, интересно. Научись создавать любые UI своими руками',
+    ],
+    options: {
+      styles: {
+        background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+        padding: '2em',
+        color: '#fff',
+        'font-height': 'bold',
+      },
+    },
+  },
+  {
+    type: 'text',
+    value: text,
+    options: {
+      styles: {
+        // background: 'linear-gradient(to right, #ff0099, #493240)',
+        background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+        padding: '1rem',
+        'font-weight': 'bold',
+      },
+    },
+  },
 ];
--- src/templates.js
+++ src/templates.js
@@ -31,7 +31,7 @@ function title(block) {
 // }
 
 function text(block) {
-  return row(col(`<p>${block.value}</p>`));
+  return row(col(`<p>${block.value}</p>`), css(block.options.styles));
 }
 
 // function columns(block) {
@@ -58,7 +58,7 @@ function columns(block) {
   // const html = block.value.map((item) => col(item));
   // return row(html.join(''));
   const html = block.value.map(col).join(''); //(item) => col(item) == col
-  return row(html);
+  return row(html, css(block.options.styles));
 }
 
 // function image(block) {
@@ -70,7 +70,11 @@ function columns(block) {
 // }
 
 function image(block) {
-  return row(`<img src="${block.value}">`);
+  const { imageStyles, alt = '', styles } = block.options;
+  return row(
+    `<img src="${block.value}" alt="${alt}" style="${css(imageStyles)}">`,
+    css(styles)
+  );
 }
 
 export const templates = {
