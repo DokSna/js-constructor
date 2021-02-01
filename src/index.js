1b2a16c946dd8f51ccd8ab52712410a83a4937d0
Artem <31787423+DokSna@users.noreply.github.com>
2021-02-01 16:34:41 +0300

    перенос цикла model.forEach из index.js в метод -> render, class Site

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { model } from './model';
+import { Site } from './classes/site';
 // // import { title, text, columns, image } from './templates';
 // import { templates } from './templates';
 import './styles/main.css';
@@ -6,27 +7,30 @@ import './styles/main.css';
 const key = 'title';
 // console.log(templates['title']);
 // получаем <div id="site"></div>
-const $site = document.querySelector('#site');
+// const $site = document.querySelector('#site');
+const site = new Site('#site');
 
-model.forEach((block) => {
-  // console.log(block.toHTML());
+site.render(model);
 
-  // let html = '';
+// model.forEach((block) => {
+//   // console.log(block.toHTML());
 
-  // if (block.type === 'title') {
-  //   html = title(block);
-  // } else if (block.type === 'text') {
-  //   html = text(block);
-  // } else if (block.type === 'columns') {
-  //   html = columns(block);
-  // } else if (block.type === 'image') {
-  //   html = image(block);
-  // }
+//   // let html = '';
 
-  // const toHTML = templates[block.type];
-    $site.insertAdjacentHTML('beforeend', block.toHTML());
-  // if (toHTML) {
-  //   // выводим в <div id="site"></div> элементы нашего массива (массив данных рендерим в код html)
-  //   $site.insertAdjacentHTML('beforeend', toHTML(block));
-  // }
-});
+//   // if (block.type === 'title') {
+//   //   html = title(block);
+//   // } else if (block.type === 'text') {
+//   //   html = text(block);
+//   // } else if (block.type === 'columns') {
+//   //   html = columns(block);
+//   // } else if (block.type === 'image') {
+//   //   html = image(block);
+//   // }
+
+//   // const toHTML = templates[block.type];
+//   site.insertAdjacentHTML('beforeend', block.toHTML());
+//   // if (toHTML) {
+//   //   // выводим в <div id="site"></div> элементы нашего массива (массив данных рендерим в код html)
+//   //   $site.insertAdjacentHTML('beforeend', toHTML(block));
+//   // }
+// });
