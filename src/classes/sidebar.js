c89548af63592498c465c23b65ebbdd89eb52ab6
Artem <31787423+DokSna@users.noreply.github.com>
2021-02-01 21:56:39 +0300

    Отменяем перезагрузку страницы, при нажатии кнопки

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -1,3 +1,4 @@
+import { block } from '../utils';
 export class Sidebar {
   constructor(selector) {
     this.$el = document.querySelector(selector);
@@ -7,32 +8,22 @@ export class Sidebar {
 
   init() {
     this.$el.insertAdjacentHTML('afterbegin', this.template);
+    // сразу после инициализации, когда мы собрали DOM, мы начинаем слушать событие submit
+    this.$el.addEventListener('submit', this.add);
   }
 
   get template() {
     return [
-      block('text'),  //добавление нескольких блоков через массив
-      block('title')
-    ].join('')
+      block('text'), //добавление нескольких блоков через массив
+      block('title'),
+    ].join('');
 
     //return block('text'); //добавление одного блока в сайдбар
 
     //// return '<h1>template</h1>';
   }
-}
 
-export function block(type) {
-  return `
-  <form name="${type}">
-    <h5>${type}</h5>
-    <div class="form-group">
-      <input class="form-control form-control-sm name="value" placeholder="value">
-    </div>
-    <div class="form-group">
-      <input class="form-control form-control-sm name="styles" placeholder="styles">
-    </div>
-    <button type="submit" class="bth bth-primary bth-sm">Добавить</button>
-  </form>
-  <hr />
-  `;
+  add(event) {
+    event.preventDefault(); //и страница не перезагружается при нажатии кнопки
+  }
 }
